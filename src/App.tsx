f8432346bd01b2484152715fa72ffa1b807457f5
giller27 <oleksandrsidorov903@gmail.com>
2025-10-20 01:51:51 +0300

    Add simple map

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Alert from "./Components/Alert";
 import Button from "./Components/Button";
 import Navbar from "./Components/Navbar";
 import Header from "./Components/Header";
+import Map from "./Components/map";
 
 function App() {
   const [showAlert, setShowAlert] = useState(false);
@@ -11,6 +12,7 @@ function App() {
       <Header></Header>
       {showAlert && <Alert onClose={() => setShowAlert(false)}>Alert</Alert>}
       <Button onClicked={() => setShowAlert(true)}>OK</Button>
+      <Map></Map>
     </div>
   );
 }
