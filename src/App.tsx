373bf3aa3200dd37478e5bcdcf52afc8a0dcdace
MaxManilko <manilko330@gmail.com>
2025-10-23 00:26:11 +0300

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 
 function App() {
-  return <Header />;
+  return (
+  <>
+  <Header />
+  <Footer />
+  </>
+  )
 }
 
 export default App;
